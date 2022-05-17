90047df85fa236babefffd5cce8cda59b3851d76
Jin2949 <97649293+Jin2949@users.noreply.github.com>
2022-05-17 21:38:01 +0900

    [Silver IV] Title: 큐 2, Time: 1208 ms, Memory: 227396 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+q = deque()
+
+for _ in range(N):
+    a = input().split()
+    if a[0] == 'push':
+        q.append(a[1])
+    elif a[0] == 'pop':
+        if not q:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif a[0] == 'size':
+        print(len(q))
+    elif a[0] == 'empty':
+        if not q:
+            print(1)
+        else:
+            print(0)
+    elif a[0] == 'front':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+    elif a[0] == 'back':
+        if not q:
+            print(-1)
+        else:
+            print(q[-1])
