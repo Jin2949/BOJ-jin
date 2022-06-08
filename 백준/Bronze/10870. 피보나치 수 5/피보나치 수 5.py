8895de245775532aaf19ba5eb77231afd7bf0be6
Jin2949 <97649293+Jin2949@users.noreply.github.com>
2022-06-08 10:23:37 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 128 ms, Memory: 116344 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(input())
+
+def fibo(num):
+    global rlt
+    if num == 0:
+       return 0
+    elif num == 1:
+        return 1
+
+    return fibo(num-1) + fibo(num-2)
+
+print(fibo(n))
